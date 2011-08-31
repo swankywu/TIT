96a3dc8036e9b1685b742df3ffc4e90751437654
swankywu <swanky.wu@gmail.com>
2011-08-31 18:39:01 +0800

    修改readme，增加2个测试

---
+++ TIT/SelectGameModeScene.h
@@ -0,0 +1,16 @@
+//
+//  SelectGameModeScene.h
+//  TIT
+//
+//  Created by swanky wu on 11-8-31.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface SelectGameModeScene : CCScene {
+    
+}
+
+@end
---
+++ TIT/SelectGameModeScene.m
@@ -0,0 +1,14 @@
+//
+//  SelectGameModeScene.m
+//  TIT
+//
+//  Created by swanky wu on 11-8-31.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SelectGameModeScene.h"
+
+
+@implementation SelectGameModeScene
+
+@end
